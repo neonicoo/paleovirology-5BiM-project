d2e83a7b155dbfc4914199dde4dfaebdb04a9e28
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2022-01-14 16:05:23 +0100

    Add bash script to install and format blast viral DB

---
+++ install_viral_prot_refseq.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# get annotations
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.gpff.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.2.protein.gpff.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.3.protein.gpff.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.4.protein.gpff.gz
+gunzip viral.1.protein.gpff.gz
+gunzip viral.2.protein.gpff.gz
+gunzip viral.3.protein.gpff.gz
+gunzip viral.4.protein.gpff.gz
+
+# download and create BLAST databases for viral refseq protein
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.faa.gz
+gunzip viral.1.protein.faa.gz
+makeblastdb -dbtype prot \
+        -parse_seqids \
+        -in viral.1.protein.faa \
+        -out viral.1.protein \
+        -title 'Virus Refseq Protein 1'
+        
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.2.protein.faa.gz
+gunzip viral.2.protein.faa.gz
+makeblastdb -dbtype prot \
+        -parse_seqids \
+        -in viral.2.protein.faa \
+        -out viral.2.protein \
+        -title 'Virus Refseq Protein 2'
+        
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.3.protein.faa.gz
+gunzip viral.3.protein.faa.gz
+makeblastdb -dbtype prot \
+        -parse_seqids \
+        -in viral.3.protein.faa \
+        -out viral.3.protein \
+        -title 'Virus Refseq Protein 3'
+
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.4.protein.faa.gz
+gunzip viral.4.protein.faa.gz
+makeblastdb -dbtype prot \
+        -parse_seqids \
+        -in viral.4.protein.faa \
+        -out viral.4.protein \
+        -title 'Virus Refseq Protein 4'
