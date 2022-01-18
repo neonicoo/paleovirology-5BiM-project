88c87410cbe142c0a418a791288a9168939029ef
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2022-01-18 10:58:29 +0100

    Add script to recognize virus from their seqid

---
+++ virus_identify.py
@@ -0,0 +1,40 @@
+#!/home/nicolas/miniconda3/bin/python3
+
+import pandas as pd
+import sys 
+import os
+
+blast = pd.read_table(sys.argv[1], header = None)
+blast.columns = ["query_seqid",
+				"subject_seqid",
+				"perc_ident",
+				"length",
+				"mismatch",
+				"gapopen",
+				"query_start",
+				"query_end",
+				"subject_start",
+				"subject_end",
+				"evalue",
+				"bitscore"]
+
+virus = pd.read_table(sys.argv[2], header = None)
+virus.columns = ["seqid",
+				"length",
+				"family",
+				"name",
+				"realm",
+				"order",
+				"autority"]
+
+
+sseqid = list(blast["subject_seqid"].values)
+sname = []
+for s in sseqid:
+    sname.append(virus["name"].loc[virus["seqid"] == s].values[0])
+
+    
+blast.insert(2,'subject_name', sname)
+blast.to_csv("./blast_virus", header=True, sep='\t')
+
+print("######## DONE #######")
