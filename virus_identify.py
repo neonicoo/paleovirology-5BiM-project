090b909e7db5c5280a234e50a8ee7e04f0998ffb
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2022-01-18 14:10:27 +0100

    Add genus column

--- virus_identify.py
+++ virus_identify.py
@@ -5,38 +5,40 @@ import sys
 import os
 
 blast = pd.read_table(sys.argv[1], header = None)
-blast.columns = ["query_seqid",
-				"subject_seqid",
-				"perc_ident",
-				"length",
-				"mismatch",
-				"gapopen",
-				"query_start",
-				"query_end",
-				"subject_start",
-				"subject_end",
-				"evalue",
-				"bitscore"]
+blast.columns = ["#query_seqid",
+				"#subject_seqid",
+				"#perc_ident",
+				"#length",
+				"#mismatch",
+				"#gapopen",
+				"#query_start",
+				"#query_end",
+				"#subject_start",
+				"#subject_end",
+				"#evalue",
+				"#bitscore"]
 
 
 virus = pd.read_table(sys.argv[2], header = None)
-virus.columns = ["seqid",
-				"length",
-				"family",
-				"name",
-				"realm",
-				"order",
-				"autority"]
+virus.columns = ["#seqid",
+				"#length",
+				"#genus",
+				"#name",
+				"#realm",
+				"#order",
+				"#autority"]
 
 output = sys.argv[3]
 
-sseqid = list(blast["subject_seqid"].values)
+sseqid = list(blast["#subject_seqid"].values)
 sname = []
+sgenus = []
 for s in sseqid:
-    sname.append(virus["name"].loc[virus["seqid"] == s].values[0])
-
+    sname.append(virus["#name"].loc[virus["#seqid"] == s].values[0])
+    sgenus.append(virus["#genus"].loc[virus["#seqid"] == s].values[0])
     
-blast.insert(2,'subject_name', sname)
+blast.insert(2,'#subject_species', sname)
+blast.insert(3,'#subject_genus', sgenus)
 blast.to_csv("./"+str(output)+".txt", header=True, sep='\t')
 
 print("######## DONE #######")
