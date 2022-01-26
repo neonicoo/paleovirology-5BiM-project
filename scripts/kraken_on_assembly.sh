e1559e76570947f7d59e808de7917ea11f505065
AdrianZBIM <62555688+AdrianZBIM@users.noreply.github.com>
2022-01-27 00:11:20 +0100

    copy of kraken outputs in kraken_reports folder

--- scripts/kraken_on_assembly.sh
+++ scripts/kraken_on_assembly.sh
@@ -4,11 +4,6 @@
 #--------------------------------------------
 # Authors: Adrian Zurmely - Maelle Broustal - Nicolas Mendiboure INSA Lyon 5BIM
 
-# Une commande ça a ce format là: 
-# kraken2 --threads 10 -db kraken_db/viral18 --unclassified-out fichier_unclassified --classified-out fichier_classified --report report --use-names fichier_source
-# ça c'est pour tester avec les kmers taille 18
-# et pour les kmers taille 15 faut prendre la db kraken_db/viral15, pour les kmers taille 11 c'est kraken_db/little_viral
-
 
 # Les fichiers en entrée de kraken sur le pipeline principal c'est dans 
 # VANA :
@@ -18,13 +13,21 @@
 # /test_data/siRNA/trimmed_cutadapt/nom_echantillon/SPAdes/compressed_SPAdes.fasta_Compressed.fa
 
 
-DOSSIER_siRNA=~/test_data/siRNA/trimmed_cutadapt/
+DOSSIER_siRNA=~/data/siRNA/trimmed_cutadapt/
 DOSSIER_VANA=~/data/VANA/trimmed_cutadapt/
 
 
 for namefileR1 in $DOSSIER_VANA/*R1*.fastq
 do 
 	namefileR1=$(basename $namefileR1 .fastq)
+	echo $namefileR1
+	mkdir ~/data/VANA/kraken_reports -p
+	mkdir ~/data/VANA/kraken_reports/SPAdes -p
+	mkdir ~/data/VANA/kraken_reports/SPAdes/$namefileR1 -p
+
+	mkdir ~/data/VANA/kraken_reports/IDBA -p
+	mkdir ~/data/VANA/kraken_reports/IDBA/$namefileR1 -p
+
 	cd $DOSSIER_VANA/$namefileR1
 	mkdir kraken -p
 	cd kraken
@@ -36,11 +39,13 @@ do
 	mkdir 3_viral
 # kraken sur recup outputMETA/contigs.fasta
 	# 1e etape
-	kraken2 --threads 8 -db ~/kraken_db/kraken2_no_viral_db --unclassified-out 1_no_viraldb/potential_viral.fa --classified-out 1_no_viraldb/non_viral.fa --report 1_no_viraldb/report_no_viral --use-names $DOSSIER_VANA/$namefileR1/outputMETA/contigs.fasta
+	kraken2 --threads 8 -db ~/kraken_db/kraken2_no_viral_db --unclassified-out 1_no_viraldb/potential_viral.fa --classified-out 1_no_viraldb/non_viral.fa --report 1_no_viraldb/report_no_viral.txt --use-names $DOSSIER_VANA/$namefileR1/outputMETA/contigs.fasta
 	# 2e etape
-	kraken2 --threads 8 -db ~/kraken_db/kraken2_plant --unclassified-out 2_plant/potential_viral.fa --classified-out 2_plant/plant.fa --report 2_plant/report_plant --use-names $DOSSIER_VANA/$namefileR1/kraken/SPAdes/1_no_viraldb/potential_viral.fa
+	kraken2 --threads 8 -db ~/kraken_db/kraken2_plant --unclassified-out 2_plant/potential_viral.fa --classified-out 2_plant/plant.fa --report 2_plant/report_plant.txt --use-names $DOSSIER_VANA/$namefileR1/kraken/SPAdes/1_no_viraldb/potential_viral.fa
 	# 3e etape 
-	kraken2 --threads 8 -db ~/kraken_db/kraken_viral --unclassified-out 3_viral/non_viral.fa --classified-out 3_viral/viral.fa --report 3_viral/report_viral --use-names $DOSSIER_VANA/$namefileR1/outputMETA/contigs.fasta
+	kraken2 --threads 8 -db ~/kraken_db/kraken_viral --unclassified-out 3_viral/non_viral.fa --classified-out 3_viral/viral.fa --report 3_viral/report_viral.txt --use-names $DOSSIER_VANA/$namefileR1/outputMETA/contigs.fasta
+	# on stocke les fichiers sorties dans /data/VANA/kraken_reports/SPAdes :
+	cp 2_plant/potential_viral.fa 2_plant/report_plant.txt 3_viral/viral.fa 3_viral/report_viral.txt ~/data/VANA/kraken_reports/SPAdes/$namefileR1/
 	
 	mkdir IDBA -p
 	cd IDBA
@@ -49,12 +54,13 @@ do
 	mkdir 3_viral	
 # kraken sur recup idba_ud_out/contig.fa
 	# 1e etape
-	kraken2 --threads 8 -db ~/kraken_db/kraken2_no_viral_db --unclassified-out 1_no_viraldb/potential_viral.fa --classified-out 1_no_viraldb/non_viral.fa --report 1_no_viraldb/report_no_viral --use-names $DOSSIER_VANA/$namefileR1/idba_ud_out/contig.fa
+	kraken2 --threads 8 -db ~/kraken_db/kraken2_no_viral_db --unclassified-out 1_no_viraldb/potential_viral.fa --classified-out 1_no_viraldb/non_viral.fa --report 1_no_viraldb/report_no_viral.txt --use-names $DOSSIER_VANA/$namefileR1/idba_ud_out/contig.fa
 	# 2e etape
-	kraken2 --threads 8 -db ~/kraken_db/kraken2_plant --unclassified-out 2_plant/potential_viral.fa --classified-out 2_plant/plant.fa --report 2_plant/report_plant --use-names $DOSSIER_VANA/$namefileR1/kraken/IDBA/1_no_viraldb/potential_viral.fa
+	kraken2 --threads 8 -db ~/kraken_db/kraken2_plant --unclassified-out 2_plant/potential_viral.fa --classified-out 2_plant/plant.fa --report 2_plant/report_plant.txt --use-names $DOSSIER_VANA/$namefileR1/kraken/IDBA/1_no_viraldb/potential_viral.fa
 	# 3e etape 
-	kraken2 --threads 8 -db ~/kraken_db/kraken_viral --unclassified-out 3_viral/non_viral.fa --classified-out 3_viral/viral.fa --report 3_viral/report_viral --use-names $DOSSIER_VANA/$namefileR1/idba_ud_out/contig.fa
-	
+	kraken2 --threads 8 -db ~/kraken_db/kraken_viral --unclassified-out 3_viral/non_viral.fa --classified-out 3_viral/viral.fa --report 3_viral/report_viral.txt --use-names $DOSSIER_VANA/$namefileR1/idba_ud_out/contig.fa
+	# on stocke les fichiers sorties dans /data/VANA/kraken_reports/IDBA :
+	cp 2_plant/potential_viral.fa 2_plant/report_plant.txt 3_viral/viral.fa 3_viral/report_viral.txt ~/data/VANA/kraken_reports/IDBA/$namefileR1/
 done
 
 
@@ -62,6 +68,10 @@ done
 for namefileR1 in $DOSSIER_siRNA/*.fastq
 do 
 	namefileR1=$(basename $namefileR1 .fastq)
+	echo $namefileR1
+	mkdir ~/data/siRNA/kraken_reports -p
+	mkdir ~/data/siRNA/kraken_reports/$namefileR1 -p
+
 	cd $DOSSIER_siRNA/$namefileR1
 	mkdir kraken -p
 	cd kraken
@@ -73,12 +83,13 @@ do
 	mkdir 3_viral
 # kraken sur recup 
 	# 1e etape
-	kraken2 --threads 8 -db ~/kraken_db/kraken2_no_viral_db --unclassified-out 1_no_viraldb/potential_viral.fa --classified-out 1_no_viraldb/non_viral.fa --report 1_no_viraldb/report_no_viral --use-names $DOSSIER_siRNA/$namefileR1/SPAdes/compressed_SPAdes.fasta_Compressed.fa
+	kraken2 --threads 8 -db ~/kraken_db/kraken2_no_viral_db --unclassified-out 1_no_viraldb/potential_viral.fa --classified-out 1_no_viraldb/non_viral.fa --report 1_no_viraldb/report_no_viral.txt --use-names $DOSSIER_siRNA/$namefileR1/SPAdes/compressed_SPAdes.fasta_Compressed.fa
 	# 2e etape
-	kraken2 --threads 8 -db ~/kraken_db/kraken2_plant --unclassified-out 2_plant/potential_viral.fa --classified-out 2_plant/plant.fa --report 2_plant/report_plant --use-names $DOSSIER_siRNA/$namefileR1/kraken/SPAdes/1_no_viraldb/potential_viral.fa
+	kraken2 --threads 8 -db ~/kraken_db/kraken2_plant --unclassified-out 2_plant/potential_viral.fa --classified-out 2_plant/plant.fa --report 2_plant/report_plant.txt --use-names $DOSSIER_siRNA/$namefileR1/kraken/SPAdes/1_no_viraldb/potential_viral.fa
 	# 3e etape 
-	kraken2 --threads 8 -db ~/kraken_db/kraken_viral --unclassified-out 3_viral/non_viral.fa --classified-out 3_viral/viral.fa --report 3_viral/report_viral --use-names $DOSSIER_siRNA/$namefileR1/SPAdes/compressed_SPAdes.fasta_Compressed.fa
-	
+	kraken2 --threads 8 -db ~/kraken_db/kraken_viral --unclassified-out 3_viral/non_viral.fa --classified-out 3_viral/viral.fa --report 3_viral/report_viral.txt --use-names $DOSSIER_siRNA/$namefileR1/SPAdes/compressed_SPAdes.fasta_Compressed.fa
+	# on stocke les fichiers de sortie dans /data/siRNA/kraken_reports/
+	cp 2_plant/potential_viral.fa 2_plant/report_plant.txt 3_viral/viral.fa 3_viral/report_viral.txt ~/data/siRNA/kraken_reports/$namefileR1/
 done
 
 
